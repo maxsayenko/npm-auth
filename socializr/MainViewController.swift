b3e7540545ad1583516688ad4e9929b0899d0c9e
Max Saienko <msaienko@ancestry.com>
2015-02-26 14:43:10 -0800

    navigation

--- socializr/MainViewController.swift
+++ socializr/MainViewController.swift
@@ -10,10 +10,21 @@ import UIKit
 
 class MainViewController: UIViewController, UITableViewDelegate {
     
+    @IBAction func ButtonClick(sender: UIButton) {
+    
+        let eventsViewController: EventsViewController = self.storyboard?.instantiateViewControllerWithIdentifier("eventsView") as EventsViewController
+        
+        self.navigationController?.pushViewController(eventsViewController, animated: true)
+        
+        println(eventsViewController)
+    }
+    
     var cellContent = ["One", "Two", "Three", "Four"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        self.navigationController?.navigationBarHidden = true
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -22,12 +33,12 @@ class MainViewController: UIViewController, UITableViewDelegate {
         // Dispose of any resources that can be recreated.
     }
     
+    // Get Cell Count
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
         return cellContent.count
-        
     }
     
+    // Populate table items
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
@@ -35,7 +46,20 @@ class MainViewController: UIViewController, UITableViewDelegate {
         cell.textLabel?.text = cellContent[indexPath.row]
         
         return cell
+    }
+    
+    // Cell Click
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        //CODE TO BE RUN ON CELL TOUCH
+        let gameViewController: EventsViewController = self.storyboard?.instantiateViewControllerWithIdentifier("eventsView") as EventsViewController
         
+        self.navigationController?.pushViewController(gameViewController, animated: true)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        println("segue")
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
     }
     
 }

f372e770fb418a9778550d3074add8cccb1063d3
Max Saienko <msaienko@ancestry.com>
2015-02-26 19:46:34 -0800

    new controllers

--- socializr/MainViewController.swift
+++ socializr/MainViewController.swift
@@ -20,7 +20,7 @@ class MainViewController: UIViewController, UITableViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationController?.navigationBarHidden = true
+        // self.navigationController?.navigationBarHidden = true
         // Do any additional setup after loading the view, typically from a nib.
     }
 

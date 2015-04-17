7a68cb976d7b8ba24db4789e09400fc391198df3
Max Saienko <msaienko@ancestry.com>
2015-04-16 22:52:32 -0700

    fixing last pieces of version conflict

--- socializr/ParseFacebook.swift
+++ socializr/ParseFacebook.swift
@@ -24,22 +24,22 @@ class ParseFacebook {
         var permissions = ["public_profile", "email"]
         
         PFFacebookUtils.logInWithPermissions(permissions) {
-            (user: PFUser!, error: NSError!) -> Void in
+            (user: PFUser?, error: NSError?) -> Void in
             if (user == nil) {
                 NSLog("The user cancelled the Facebook login (user is nil)")
             } else {
                 NSLog("The user successfully logged in with Facebook (user is NOT nil)")
                 // HERE I SET AN ACL TO THE INSTALLATION
                 //TODO: Check if we need this
-//                if let installation = PFInstallation.currentInstallation() {
-//                    let acl = PFACL(user: PFUser.currentUser()) // Only user can write
-//                    acl.setPublicReadAccess(true) // Everybody can read
-//                    acl.setWriteAccess(true, forRoleWithName: "Admin") // Also Admins can write
-//                    installation.ACL = acl
-//                    installation.saveEventually({ (success:Bool, error:NSError!) -> Void in
-//                        println("Save eventually logInWithFacebook. Success=\(success)")
-//                    })
-//                }
+                //                if let installation = PFInstallation.currentInstallation() {
+                //                    let acl = PFACL(user: PFUser.currentUser()) // Only user can write
+                //                    acl.setPublicReadAccess(true) // Everybody can read
+                //                    acl.setWriteAccess(true, forRoleWithName: "Admin") // Also Admins can write
+                //                    installation.ACL = acl
+                //                    installation.saveEventually({ (success:Bool, error:NSError!) -> Void in
+                //                        println("Save eventually logInWithFacebook. Success=\(success)")
+                //                    })
+                //                }
                 // THEN I GET THE USERNAME AND fbId
                 ParseFacebook.obtainUserNameAndFbId()
                 
@@ -62,7 +62,8 @@ class ParseFacebook {
         
         let user = PFUser.currentUser() // Won't be nil because is logged in
         // RETURN IF WE ALREADY HAVE A USERNAME AND FBID (note that we check the fbId because Parse automatically fills in the username with random numbers)
-        if let fbId = user["fbId"] as? String {
+        
+        if let fbId = user?["fbId"] as? String {
             if (!fbId.isEmpty) {
                 println("we already have a username and fbId -> return")
                 return
@@ -83,9 +84,9 @@ class ParseFacebook {
                         println(result)
                         // You have 2 ways to access the result:
                         // 1)
-//                        println(result["name"])
-//                        println(result["id"])
-//                        println(result["gender"])
+                        //                        println(result["name"])
+                        //                        println(result["id"])
+                        //                        println(result["gender"])
                         // 2)
                         //println(result.name)
                         //println(result.objectID)
@@ -95,15 +96,16 @@ class ParseFacebook {
                         PFUser.currentUser()!.setValue(result["gender"], forKey: "gender")
                         PFUser.currentUser()!.setValue(result.objectID, forKey: "fbId")
                         
+                        
                         // Always use saveEventually if you want to be sure that the save will succeed
-                        PFUser.currentUser().saveEventually({ (success:Bool, error:NSError!) -> Void in
+                        PFUser.currentUser()?.saveEventually({ (success:Bool, error:NSError?) -> Void in
                             println("Save eventually. Success=\(success)")
                         })
                     }
                 })
             }
         }
-
+        
     }
     
 }

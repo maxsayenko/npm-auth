9b3e802e0e194f22dce88450757ce5aa01bedf5d
Max Saienko <msaienko@ancestry.com>
2015-10-08 01:15:23 -0700

    adding new storyboard and controllerx

---
+++ socializr/EULAViewController.swift
@@ -0,0 +1,16 @@
+//
+//  EULAViewController.swift
+//  socializr
+//
+//  Created by Max Saienko on 10/8/15.
+//  Copyright © 2015 Max Saienko. All rights reserved.
+//
+
+import UIKit
+
+@available(iOS 8.0, *)
+class EULAViewController: UIViewController {
+    override func viewDidAppear(animated: Bool) {
+        
+    }
+}
